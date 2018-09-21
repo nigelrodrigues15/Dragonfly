18068e2d03c8fef33c116b773f9685579999038b
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-21 16:00:49 -0700

    update index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/root";
+import configureStore from "./store/store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+document.addEventListener('DOMContentLoaded', () => {
+
+            const root = document.getElementById("root");
+            let store = configureStore();
+
+            ReactDOM.render( < Root store = { store }
+                />, root);
+            });
