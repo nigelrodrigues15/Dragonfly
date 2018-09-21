80684dc2858e6e1baea17670e810325dfbeb91d6
Nigel Rodrigues <nigel1@ualberta.ca>
2018-09-21 16:30:26 -0700

    file restructure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,8 @@ import Root from "./components/root";
 import configureStore from "./store/store";
 
 document.addEventListener('DOMContentLoaded', () => {
-
             const root = document.getElementById("root");
             let store = configureStore();
 
-            ReactDOM.render( < Root store = { store }
-                />, root);
+    ReactDOM.render( < Root store = { store }/>, root);
             });
